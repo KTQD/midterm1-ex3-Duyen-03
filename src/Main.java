d6639aad00ef08c4be2d48c1a3148b64485cfa4f
Duyen-03 <157661508+Duyen-03@users.noreply.github.com>
2024-03-14 08:28:40 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+    String inputFilePath = "input.txt";
+    String outputFilePath = "output.txt";
+    File inputFile = new File(inputFilePath);
+    try {
+        Scanner scanner = new Scanner(inputFile);
+            PrintWriter writer = new PrintWriter(outputFilePath);
 
-
-        Bài làm viết tại đây
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String replacedLine = line.replace("Nha Trang", "Vũng Tàu");
+                   writer.println(replacedLine);
+            }
+        scanner.close();
+            writer.close();
+        } catch (FileNotFoundException e) {
+        System.err.println("File không tìm thấy: " + e.getMessage());
+        }
 
     }
 }
